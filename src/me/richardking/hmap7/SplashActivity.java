320132f40e27af5db43ff39d73f0c3f08f108af5
Richard King <richard@richardking.me>
2012-08-16 00:03:52 +0100

    SplashActivity written, xml files for additional layouts added.

--- src/me/richardking/hmap7/SplashActivity.java
+++ src/me/richardking/hmap7/SplashActivity.java
@@ -1,13 +1,69 @@
 package me.richardking.hmap7;
 
+import me.richardking.hmap7.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class SplashActivity extends Activity {
+/*
+ * Splash screen to run on first launch. Redirects to the main Activity
+ * after the number of ms specified by the splashTime variable (int)
+ */
+
+public class SplashActivity extends Activity
+{
+	
+	protected boolean _active = true; // Whether or not we're at 1st load, which we will be initially. Also, tautologies are tautologous...
+	protected int _splashTime = 2500; // The length of time, in milliseconds, to display the splash screen.
+	
     /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+    @Override public void onCreate(Bundle savedInstanceState)
+    {
+    	
+    	// Going full screen!
+    	this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); // This is for the bar with the clock and battery status.
+    	this.requestWindowFeature(Window.FEATURE_NO_TITLE); // This is for the bar with the title in it.
+    	
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.splash); // Set the layout to the splash one.
+        
+        
+        /*
+         *  Create a thread to run for the duration of _spashTime, then set the _active
+         *  parameter to false and finally, after killing the thread, start the MainActivity
+         */
+        
+        Thread splashTread = new Thread()
+        {
+            @Override public void run() // Overriding the default function of the Thread (which is called automatically by the Thread's start() function
+            {
+                try // needed for the sleep(Integer) function.
+                {
+                	int waited = 0; // We haven't waited at all up to this point
+                    while(_active && (waited < _splashTime)) // keep going so long as we haven't waited _splashTime ms or more...
+                    {
+                    	sleep(100); // Pause for 100ms
+                        waited += 100; // We have waited (another)? 100ms
+                        _active = (waited < _splashTime); // Finished our first load stuff
+                    }
+                }
+                catch(InterruptedException e)
+                {
+                    // do nothing in the event that the sleeping in interrupted (not gonna happen *GULP*)...
+                }
+                finally
+                {
+                    finish(); // Close up the thread
+                    Intent i = new Intent("me.richardking.hmap7.MainActivity"); // Create the new intent
+                    startActivity(i); // Start a new activity from the intent
+                }
+            }
+        };
+        
+        splashTread.start(); // Go for the looping thread!
+        
     }
+    
 }
