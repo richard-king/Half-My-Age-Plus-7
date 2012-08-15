f097178f3334a84a10d851e76e4c8729269a13a4
Richard King <richard@richardking.me>
2012-08-16 00:31:47 +0100

    Basic functionality added to MainActivity

--- src/me/richardking/hmap7/MainActivity.java
+++ src/me/richardking/hmap7/MainActivity.java
@@ -2,6 +2,8 @@ package me.richardking.hmap7;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity
 {
@@ -10,9 +12,27 @@ public class MainActivity extends Activity
     @Override public void onCreate(Bundle savedInstanceState)
     {
     	
+    	// We don't need no title bar. (Hey! Coder! Leave that status bar alone!) 
+    	this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+    	
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     	
+    }
+    
+    /** Calculate the lower limit based on an integer age */
+    private int calculateLower(int myAge)
+    {
+    	if( myAge < 23 )
+    		return 18; // We don't want THE LAW a-comin' after us...
+    	
+    	return (int) ( Math.ceil(myAge/2d) + 7 );
+    }
+    
+    /** Calculate the upper limit based on an integer age */
+    private int calculateUpper(int myAge)
+    {
+    	return 2 * ( myAge - 7 );
     }
 	
 }
