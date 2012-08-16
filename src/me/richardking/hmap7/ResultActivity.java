5c13ad230100cda136dd478ce1ffe73bfc328057
Richard King <richard@richardking.me>
2012-08-17 00:09:46 +0100

    Completed UI for ResultActivity

--- src/me/richardking/hmap7/ResultActivity.java
+++ src/me/richardking/hmap7/ResultActivity.java
@@ -1,13 +1,19 @@
 package me.richardking.hmap7;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Window;
 import android.widget.TextView;
 
 public class ResultActivity extends Activity {
 
-	private TextView _tv;
+	private TextView _tv1;
+	private TextView _tv2;
+	private TextView _tv3;
+	private TextView _tv4;
+	private TextView _header;
 	
 	/** Called when the activity is first created. */
     @Override public void onCreate(Bundle savedInstanceState)
@@ -17,11 +23,41 @@ public class ResultActivity extends Activity {
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.result);
         
+        _tv1 = (TextView) findViewById(R.id.lower_limit);
+        _tv2 = (TextView) findViewById(R.id.lower_val);
+        _tv3 = (TextView) findViewById(R.id.upper_limit);
+        _tv4 = (TextView) findViewById(R.id.upper_val);
+        
         Bundle extras = getIntent().getExtras();
         
-        _tv = (TextView) findViewById(R.id.textView1);
+        String minimum = Integer.toString(extras.getInt("minimum"));
+        String maximum = Integer.toString(extras.getInt("maximum"));
         
-        _tv.setText(_tv.getText() + Integer.toString(extras.getInt("minimum")) + "-" + Integer.toString(extras.getInt("maximum")));
+        _tv2.setText(minimum);
+        _tv4.setText(maximum);
+        
+        _header = (TextView) findViewById(R.id.maintitle);
+        
+        Typeface unicorn = Typeface.createFromAsset(getAssets(), "fonts/UNICORN.TTF");
+        _header.setTypeface(unicorn);
+        _header.setTextColor(Color.WHITE);
+        _header.setTextSize(30.0f);
+        
+        _tv1.setTypeface(unicorn);
+        _tv1.setTextSize(25.0f);
+        _tv1.setTextColor(Color.parseColor("#8C006C"));
+        
+        _tv2.setTypeface(unicorn);
+        _tv2.setTextSize(100.0f);
+        _tv2.setTextColor(Color.parseColor("#8C006C"));
+        
+        _tv3.setTypeface(unicorn);
+        _tv3.setTextSize(25.0f);
+        _tv3.setTextColor(Color.parseColor("#8C006C"));
+        
+        _tv4.setTypeface(unicorn);
+        _tv4.setTextSize(100.0f);
+        _tv4.setTextColor(Color.parseColor("#8C006C"));
     }
 
 }
